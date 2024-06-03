f6d57098bf6ae82e50b96163737475dfd1967e43
Franco Valledor <francovalledor@gmail.com>
2024-06-02 21:33:38 -0300

    initial data json

---
+++ server/src/data/initialData.ts
@@ -0,0 +1,343 @@
+export const initialData = [
+  {
+    name: "Batman",
+    secretName: "Bruce Wayne",
+    appearanceYear: 1939,
+    company: "DC Comics",
+    biography:
+      "Batman es un superhéroe que lucha contra el crimen en Gotham City utilizando su intelecto, habilidades de detective y tecnología avanzada.",
+    equipment: "Batsuit, Batmobile, Batarangs, Batcomputer",
+  },
+  {
+    name: "Superman",
+    secretName: "Clark Kent",
+    appearanceYear: 1938,
+    company: "DC Comics",
+    biography:
+      "Superman es un superhéroe de Krypton que protege a la Tierra con sus poderes sobrehumanos y su sentido de la justicia.",
+    equipment: "Traje kryptoniano",
+  },
+  {
+    name: "Wonder Woman",
+    secretName: "Diana Prince",
+    appearanceYear: 1941,
+    company: "DC Comics",
+    biography:
+      "Wonder Woman es una princesa amazona y guerrera inmortal dedicada a la paz y la justicia.",
+    equipment: "Lazo de la Verdad, Brazaletes de Sumisión, Tiara",
+  },
+  {
+    name: "Flash",
+    secretName: "Barry Allen",
+    appearanceYear: 1956,
+    company: "DC Comics",
+    biography:
+      "Flash es el hombre más rápido del mundo, capaz de moverse a velocidades increíbles.",
+    equipment: "Traje de velocidad",
+  },
+  {
+    name: "Green Lantern",
+    secretName: "Hal Jordan",
+    appearanceYear: 1959,
+    company: "DC Comics",
+    biography:
+      "Green Lantern es un miembro de una fuerza policial intergaláctica que usa un anillo de poder para crear construcciones de luz sólida.",
+    equipment: "Anillo de poder",
+  },
+  {
+    name: "Aquaman",
+    secretName: "Arthur Curry",
+    appearanceYear: 1941,
+    company: "DC Comics",
+    biography:
+      "Aquaman es el rey de la Atlántida con la capacidad de comunicarse con la vida marina y super fuerza.",
+    equipment: "Tridente de Poseidón",
+  },
+  {
+    name: "Cyborg",
+    secretName: "Victor Stone",
+    appearanceYear: 1980,
+    company: "DC Comics",
+    biography:
+      "Cyborg es un superhéroe cibernético con fuerza sobrehumana y capacidades tecnológicas avanzadas.",
+    equipment: "Tecnología cibernética",
+  },
+  {
+    name: "Green Arrow",
+    secretName: "Oliver Queen",
+    appearanceYear: 1941,
+    company: "DC Comics",
+    biography:
+      "Green Arrow es un arquero experto que lucha contra el crimen con una variedad de flechas trucadas.",
+    equipment: "Arco y flechas trucadas",
+  },
+  {
+    name: "Martian Manhunter",
+    secretName: "J'onn J'onzz",
+    appearanceYear: 1955,
+    company: "DC Comics",
+    biography:
+      "Martian Manhunter es un marciano con habilidades de cambio de forma, telepatía y vuelo.",
+    equipment: "Ninguno",
+  },
+  {
+    name: "Black Canary",
+    secretName: "Dinah Lance",
+    appearanceYear: 1947,
+    company: "DC Comics",
+    biography:
+      "Black Canary es una experta luchadora con la habilidad de emitir un grito sónico destructivo.",
+    equipment: "Ninguno",
+  },
+  {
+    name: "Spider-Man",
+    secretName: "Peter Parker",
+    appearanceYear: 1962,
+    company: "Marvel Comics",
+    biography:
+      "Spider-Man es un joven con habilidades arácnidas que protege a Nueva York del crimen.",
+    equipment: "Lanzadores de telaraña",
+  },
+  {
+    name: "Iron Man",
+    secretName: "Tony Stark",
+    appearanceYear: 1963,
+    company: "Marvel Comics",
+    biography:
+      "Iron Man es un genio multimillonario que lucha contra el mal con su traje de alta tecnología.",
+    equipment: "Traje de Iron Man",
+  },
+  {
+    name: "Captain America",
+    secretName: "Steve Rogers",
+    appearanceYear: 1941,
+    company: "Marvel Comics",
+    biography:
+      "Captain America es un supersoldado de la Segunda Guerra Mundial que lucha por la libertad y la justicia.",
+    equipment: "Escudo indestructible",
+  },
+  {
+    name: "Thor",
+    secretName: "Thor Odinson",
+    appearanceYear: 1962,
+    company: "Marvel Comics",
+    biography:
+      "Thor es el dios del trueno de Asgard, conocido por su fuerza y su martillo encantado, Mjolnir.",
+    equipment: "Mjolnir",
+  },
+  {
+    name: "Hulk",
+    secretName: "Bruce Banner",
+    appearanceYear: 1962,
+    company: "Marvel Comics",
+    biography:
+      "Hulk es un científico que se transforma en un monstruo verde con fuerza sobrehumana cuando se enfurece.",
+    equipment: "Ninguno",
+  },
+  {
+    name: "Black Widow",
+    secretName: "Natasha Romanoff",
+    appearanceYear: 1964,
+    company: "Marvel Comics",
+    biography:
+      "Black Widow es una espía y experta luchadora que trabaja para SHIELD y los Avengers.",
+    equipment: "Bastones electrificados, Dispositivos de espionaje",
+  },
+  {
+    name: "Doctor Strange",
+    secretName: "Stephen Strange",
+    appearanceYear: 1963,
+    company: "Marvel Comics",
+    biography:
+      "Doctor Strange es un hechicero supremo que protege la Tierra de amenazas mágicas y místicas.",
+    equipment: "Capa de Levitación, Ojo de Agamotto",
+  },
+  {
+    name: "Wolverine",
+    secretName: "Logan",
+    appearanceYear: 1974,
+    company: "Marvel Comics",
+    biography:
+      "Wolverine es un mutante con factor de curación y garras retráctiles de adamantium.",
+    equipment: "Garras de adamantium",
+  },
+  {
+    name: "Daredevil",
+    secretName: "Matt Murdock",
+    appearanceYear: 1964,
+    company: "Marvel Comics",
+    biography:
+      "Daredevil es un abogado ciego que lucha contra el crimen usando sus sentidos agudizados.",
+    equipment: "Bastón billy",
+  },
+  {
+    name: "Black Panther",
+    secretName: "T'Challa",
+    appearanceYear: 1966,
+    company: "Marvel Comics",
+    biography:
+      "Black Panther es el rey de Wakanda, un país africano tecnológicamente avanzado, y un hábil guerrero.",
+    equipment: "Traje de vibranium",
+  },
+  {
+    name: "Captain Marvel",
+    secretName: "Carol Danvers",
+    appearanceYear: 1968,
+    company: "Marvel Comics",
+    biography:
+      "Captain Marvel es una ex piloto de la Fuerza Aérea con poderes cósmicos y super fuerza.",
+    equipment: "Ninguno",
+  },
+  {
+    name: "Ant-Man",
+    secretName: "Scott Lang",
+    appearanceYear: 1979,
+    company: "Marvel Comics",
+    biography:
+      "Ant-Man es un ladrón reformado que puede encogerse al tamaño de una hormiga y comunicarse con insectos.",
+    equipment: "Traje de Ant-Man",
+  },
+  {
+    name: "Scarlet Witch",
+    secretName: "Wanda Maximoff",
+    appearanceYear: 1964,
+    company: "Marvel Comics",
+    biography:
+      "Scarlet Witch es una mutante con habilidades de manipulación de la realidad y poderes mágicos.",
+    equipment: "Ninguno",
+  },
+  {
+    name: "Vision",
+    secretName: "Vision",
+    appearanceYear: 1968,
+    company: "Marvel Comics",
+    biography:
+      "Vision es un androide sintético creado por Ultron, con la capacidad de volar y alterar su densidad.",
+    equipment: "Gema de la mente",
+  },
+  {
+    name: "Hawkeye",
+    secretName: "Clint Barton",
+    appearanceYear: 1964,
+    company: "Marvel Comics",
+    biography:
+      "Hawkeye es un arquero maestro y miembro de los Avengers, conocido por su puntería perfecta.",
+    equipment: "Arco y flechas",
+  },
+  {
+    name: "Falcon",
+    secretName: "Sam Wilson",
+    appearanceYear: 1969,
+    company: "Marvel Comics",
+    biography:
+      "Falcon es un aliado de Captain America con la capacidad de volar gracias a sus alas mecánicas.",
+    equipment: "Ala de vuelo",
+  },
+  {
+    name: "Luke Cage",
+    secretName: "Carl Lucas",
+    appearanceYear: 1972,
+    company: "Marvel Comics",
+    biography:
+      "Luke Cage es un héroe de alquiler con fuerza sobrehumana y piel impenetrable.",
+    equipment: "Ninguno",
+  },
+  {
+    name: "Jessica Jones",
+    secretName: "Jessica Jones",
+    appearanceYear: 2001,
+    company: "Marvel Comics",
+    biography:
+      "Jessica Jones es una investigadora privada con fuerza sobrehumana y un pasado trágico.",
+    equipment: "Ninguno",
+  },
+  {
+    name: "Star-Lord",
+    secretName: "Peter Quill",
+    appearanceYear: 1976,
+    company: "Marvel Comics",
+    biography:
+      "Star-Lord es un aventurero espacial y líder de los Guardianes de la Galaxia.",
+    equipment: "Cascos, Armas de energía",
+  },
+  {
+    name: "Gamora",
+    secretName: "Gamora",
+    appearanceYear: 1975,
+    company: "Marvel Comics",
+    biography:
+      "Gamora es la mujer más peligrosa del universo y miembro de los Guardianes de la Galaxia.",
+    equipment: "Espada Godslayer",
+  },
+  {
+    name: "Rocket Raccoon",
+    secretName: "Rocket",
+    appearanceYear: 1976,
+    company: "Marvel Comics",
+    biography:
+      "Rocket Raccoon es un mapache cibernético experto en armas y tácticas.",
+    equipment: "Armas de fuego avanzadas",
+  },
+  {
+    name: "Groot",
+    secretName: "Groot",
+    appearanceYear: 1960,
+    company: "Marvel Comics",
+    biography:
+      "Groot es un ser alienígena con la capacidad de regenerarse y manipular madera.",
+    equipment: "Ninguno",
+  },
+  {
+    name: "Drax",
+    secretName: "Drax the Destroyer",
+    appearanceYear: 1973,
+    company: "Marvel Comics",
+    biography:
+      "Drax es un guerrero con fuerza sobrehumana y un deseo de venganza contra Thanos.",
+    equipment: "Cuchillas dobles",
+  },
+  {
+    name: "Storm",
+    secretName: "Ororo Munroe",
+    appearanceYear: 1975,
+    company: "Marvel Comics",
+    biography: "Storm es una mutante con la habilidad de controlar el clima.",
+    equipment: "Ninguno",
+  },
+  {
+    name: "Cyclops",
+    secretName: "Scott Summers",
+    appearanceYear: 1963,
+    company: "Marvel Comics",
+    biography:
+      "Cyclops es el líder de campo de los X-Men, capaz de disparar rayos ópticos de energía.",
+    equipment: "Visor de cuarzo rubí",
+  },
+  {
+    name: "Jean Grey",
+    secretName: "Jean Grey",
+    appearanceYear: 1963,
+    company: "Marvel Comics",
+    biography:
+      "Jean Grey es una poderosa mutante con habilidades telepáticas y telequinéticas.",
+    equipment: "Ninguno",
+  },
+  {
+    name: "Beast",
+    secretName: "Hank McCoy",
+    appearanceYear: 1963,
+    company: "Marvel Comics",
+    biography:
+      "Beast es un mutante con apariencia bestial y una mente brillante.",
+    equipment: "Ninguno",
+  },
+  {
+    name: "Nightcrawler",
+    secretName: "Kurt Wagner",
+    appearanceYear: 1975,
+    company: "Marvel Comics",
+    biography:
+      "Nightcrawler es un mutante con la habilidad de teletransportarse y una apariencia demoníaca.",
+    equipment: "Ninguno",
+  },
+];
