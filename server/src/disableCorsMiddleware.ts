1880e849f93a6675662cd357e12c9baee86c0b34
Franco Valledor <franco.valledor@bluealba.com>
2024-12-10 21:21:16 -0300

    disable CORS

--- server/src/disableCorsMiddleware.ts
+++ server/src/disableCorsMiddleware.ts
@@ -2,6 +2,15 @@ import { RequestHandler } from "express";
 
 export const disableCorsMiddleware: RequestHandler = (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+  if (req.method === "OPTIONS") {
+    res.sendStatus(204);
+  } else {
+    next();
+  }
 };
